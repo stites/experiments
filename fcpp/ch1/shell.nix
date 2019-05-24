f279936341fd0b75c3ae03152b80ab2128439b48
Sam Stites <stites@users.noreply.github.com>
2019-05-23 22:15:54 -0400

    move shell.nix to more local scope

--- fcpp/ch1/shell.nix
+++ fcpp/ch1/shell.nix
@@ -1,8 +1,8 @@
 with (import <nixpkgs> {});
 
 let
-  fpcpp-ch1 = callPackage ./ch1 {};
-  ranges-v3 = callPackage ./ch1/ranges-v3.nix {};
+  fpcpp-ch1 = callPackage ./. {};
+  ranges-v3 = callPackage ./ranges-v3.nix {};
 in
 mkShell {
   buildInputs = [ fpcpp-ch1 ranges-v3 ];
