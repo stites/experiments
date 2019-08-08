8098ae7a983756578909a559ae611d4c7356b3da
Sam Stites <stites@users.noreply.github.com>
2019-08-08 16:12:57 -0400

    look at natural transformations

--- adj2mon/src/Lib.hs
+++ adj2mon/src/Lib.hs
@@ -1,6 +1,58 @@
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE InstanceSigs #-}
 module Lib
     ( someFunc
     ) where
 
+import Data.Kind
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- functor composition is defined for
+-- F: A -> B
+-- G: B -> C
+-- as G * F : A -> C
+
+newtype FComp f g a = C { unC :: f (g a) }
+
+instance (Functor f, Functor g) => Functor (FComp f g) where
+  fmap :: (a -> b) -> FComp f g a -> FComp f g b
+  fmap f (C x) = C $ (fmap.fmap) f x
+
+-- The identity functor maps each morphism and object onto itself
+newtype Identity a = Id { unId :: a }
+
+instance Functor Identity where
+  fmap f (Id x) = Id (f x)
+
+-- composition with the identity functor forms an identity relation:
+--     F * (Id_A)
+-- (Id_B)   F
+
+left :: Functor f => FComp f Identity a -> f a
+left (C a) = fmap unId a
+
+right :: Functor f => f a -> FComp f Identity a
+right a = C $ fmap Id a
+
+-- Given a Natural Transformation
+type Nat f g = forall a . f a -> g a
+-- The interchage law states taht we can chase a natural transformation through functors
+-- horizonally, or compose natural transformations between functors vertically and still
+-- arrive at the same result:
+type NatComp f f' g g' = forall a . f' (f a) -> g' (g a)
+
+vert
+  :: (Functor f, Functor f')
+  => (Functor g, Functor g')
+  => Nat f' g' -> Nat f g -> NatComp f f' g g'
+vert a b x = a (fmap b x)
+
+horiz
+  :: (Functor f, Functor f')
+  => (Functor g, Functor g')
+  => Nat f' g' -> Nat f g -> NatComp f f' g g'
+horiz a b x = fmap b (a x)
+
--- adj2mon/test/Spec.hs
+++ adj2mon/test/Spec.hs
@@ -1,2 +1,28 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE TemplateHaskell #-}
+import Hedgehog
+import Hedgehog.Main
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Gen.QuickCheck as Gen
+
+-- Functor laws
+type MyFunctor = Maybe
+type MyFunctorInst = MyFunctor Int
+
+a = (+2)
+b = (*(-1))
+
+prop_functor_identity :: Property
+prop_functor_identity = property $ do
+  xs <- forAll $ (Gen.arbitrary :: Gen MyFunctorInst)
+  fmap id xs === id xs
+
+prop_functor_composition :: Property
+prop_functor_composition = property $ do
+  xs <- forAll $ (Gen.arbitrary :: Gen MyFunctorInst)
+  fmap (a . b) xs === ((fmap a) . (fmap b) $ xs)
+
+tests :: IO Bool
+tests =
+  checkParallel $$(discover)
+
+main = defaultMain [tests]
